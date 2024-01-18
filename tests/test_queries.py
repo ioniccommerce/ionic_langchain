8d11a4c230b9086e827464bde4f24bafa432db22
Jarod Stewart <jarod@jarod.is>
2024-01-18 13:53:50 -0700

    wip

---
+++ tests/test_queries.py
@@ -0,0 +1,43 @@
+import sys
+
+import pytest
+from ionic import Ionic as IonicSdk
+from ionic.models.errors import HTTPValidationError
+
+from ionic_langchain.tool import Ionic, Query, QueryInput
+
+
+def test_ionic_num_results():
+    """
+    requires server to be running (i.e. not compatible with CI)
+    """
+    ionic = Ionic(
+        sdk=IonicSdk(server_url="http://localhost:8080"),
+    )
+    results = ionic.query(
+        query_input=QueryInput(
+            queries=[
+                Query(
+                    query="Reindeer Jerky",
+                    num_results=2,
+                ),
+                Query(
+                    query="Bison Jerky",
+                    num_results=2,
+                ),
+            ]
+        )
+    )
+
+    assert len(results) == 1
+    reindeer_jerky_result = results[0]
+    assert (
+        reindeer_jerky_result["query"]["query"] == "Reindeer Jerky"
+    ), "query should be included in response object"
+    assert reindeer_jerky_result["query"]["num_results"] == 2
+    assert reindeer_jerky_result["query"]["max_price"] is None
+    assert reindeer_jerky_result["query"]["min_price"] is None
+    assert "products" in reindeer_jerky_result
+    assert (
+        len(reindeer_jerky_result["products"]) == 2
+    ), "num_results should be respected"
