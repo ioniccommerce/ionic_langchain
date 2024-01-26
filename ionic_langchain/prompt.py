6c3e9a9d0b6cf1e02230b1748a7d141a2514c109
Jarod Stewart <jarod@jarod.is>
2024-01-26 09:35:18 -0700

    Prompt agent to use full pdp url

--- ionic_langchain/prompt.py
+++ ionic_langchain/prompt.py
@@ -3,5 +3,6 @@ TOOL_PROMPT = str("""
 
     The user can specify the number of results, minimum price, and maximum price that they want to see results for.
     Ionic Tool input is a comma seperated string of values: query (required), number of results (default to 5), minimum price in cents, and maximum price in cents. For example `coffee beans, 5, 500, 1000` or `coffee beans, 5` or `coffee beans, 5, 500` or `coffee beans, 5, , 1000`. If the user has not specified how many results they want, use 5 as the default.
-    Convert the price to cents before passing to the tool. For example, if the user asks for a product between $5 and $10, you would pass `500` and `1000` to the tool.  
+    Convert the price to cents before passing to the tool. For example, if the user asks for a product between $5 and $10, you would pass `500` and `1000` to the tool.
+    Use the full pdp type url when presenting to users.
 """)
