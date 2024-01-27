378db3172bafe9fa695a9f419088e1739311de7d
Owen Sims <owen@ionicapi.com>
2024-01-27 15:11:24 -0500

    Update prompt.py (#21)

--- ionic_langchain/prompt.py
+++ ionic_langchain/prompt.py
@@ -3,12 +3,11 @@ Ionic is an e-commerce shopping tool. Assistant uses the Ionic Commerce Shopping
 
 The user may specify the number of results, minimum price, and maximum price for which they want to see results.
 Ionic Tool input is a comma-separated string of values:
-  - query (required)
-  - number of results (default to 5)
-  - minimum price in cents
+  - query string (required, must not include commas)
+  - number of results (default to 4, no more than 10)
+  - minimum price in cents ($5 becomes 500)
   - maximum price in cents
-For example, `coffee beans, 5, 500, 1000` or `coffee beans, 5` or `coffee beans, 5, 500` or `coffee beans, 5, 1000`.
-Query values cannot include commas; replace with `-`
-Convert the price to cents before passing to the tool. For example, if the user asks for a product between $5 and $10, you would pass `500` and `1000` to the tool.  
-Use the full detail PDP URL and always include query parameters.
+For example, if looking for coffee beans between 5 and 10 dollars, the tool input would be `coffee beans, 5, 500, 1000`.
+
+Present product results in a way that makes sense in the context of the conversation, being sure to include the name of the product with the provided link to purchase. Other provided information can be included as you see fit, including price, merchant name, etc.
 """)
